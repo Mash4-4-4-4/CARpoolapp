50c158cb9c42cf0d3a6a7ad19837eb47bacfe675
Mash4-4-4-4 <maniska5740@gmail.com>
2026-02-03 15:43:36 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,56 @@
-package backend;
+package carpool;
+import java.util.List;
+import java.util.Scanner;
+public class Main 
+{
+  public static void main(String args[])
+  {
+	  Scanner sc=new Scanner(System.in);
+	  RideBooking app=new RideBooking();
+	  app.addRide(new Ride("Noida", "Delhi", 1, 3, 100));
+      app.addRide(new Ride("Gurgaon", "Delhi", 2, 2, 150));
+      
+      System.out.println("enter email: ");
+      String email=sc.next();
+      
+      System.out.println("enter Password: ");
+      String password=sc.next();
+      
+      User user=app.signin(email, password);
+      
+      if(user==null)
+      {
+    	  System.out.println("User not found. Registering...");
+          System.out.print("Enter name: ");
+          String name=sc.next();
+          user=app.Register(name, email, password);
+          System.out.println("Welcome, " + user.getName());
+          
+          System.out.print("Enter source: ");
+          String source = sc.next();
 
-	public class Main {
-		public static void main(String[] args) {
-				RideBooking rideBookingSystem= new RideBooking();
-				rideBookingSystem.createRide(1,"Delhi","Greater Noida",2,250.00);
-				rideBookingSystem.createRide(2,"Jaipur","Ajmer",3,420.00);
-				rideBookingSystem.createRide(3,"Gurugram","delhi",5,750);
-				System.out.println(rideBookingSystem.showAllRides());
-			}
+          System.out.print("Enter destination: ");
+          String destination = sc.next();
 
-		}
-
+          System.out.print("Enter seats required: ");
+          int seats = sc.nextInt();
+          List<Ride>available=app.searchrides(source, destination, seats);
+          if(available.isEmpty())
+          {
+        	  System.out.println("no seats available");
+        	  return ;
+          }
+          Ride selectedRide=available.get(0);
+          System.out.println("ride found:");
+          
+          Booking booking=app.bookRide(user, selectedRide, seats);
+          
+          if(booking!=null)
+          {
+        	  System.out.println("ride booked successfully");
+        	  System.out.println(booking);
+          }
+      }
+      
+  }
+}
